17536f478b56e236b2dafa79bfc3c9f6fc03ca76
Bartłomiej Kramnik <bkramnik@advatech.pl>
2019-05-27 15:50:52 +0200

    wiecej parametrów do modyfikacji

--- network.py
+++ network.py
@@ -13,25 +13,26 @@ def read_data():
     return load_data
 
 
-def separate_sets(data):
-    sets_border = int(random.randint(0, int(data.shape[0] * 0.7)))
-    test_set_size = int(data.shape[0] * 0.3)
+def separate_sets(data, test_set=0.3):
+    sets_border = int(random.randint(0, int(data.shape[0] * (1.0-test_set))))
+    test_set_size = int(data.shape[0] * test_set)
     test_data = data[sets_border:sets_border + test_set_size, :]
     train_data = numpy.append(data[0:sets_border, :], data[sets_border + test_set_size:data.shape[0] - 1, :], axis=0)
 
     return train_data[:, 1:], train_data[:, 0], test_data[:, 1:], test_data[:, 0]
 
 
-def create_model(hidden_layer_neurons=16, dropout=0.2, learning_rate=0.4, momentum=0.09, learning_rate_decay=0.01):
+def create_model(hidden_layer_neurons=16, dropout=0.2, learning_rate=0.4, momentum=0.09, learning_rate_decay=0.01,
+                 attributes=30):
     model = Sequential()
 
     # warstwa normlizacji
-    model.add(Dense(units=30, input_dim=30, init='uniform'))
+    model.add(Dense(units=attributes, input_dim=attributes, init='uniform'))
     model.add(BatchNormalization())
     model.add(Activation('tanh'))
 
     # warstwa ukryta
-    model.add(Dense(units=hidden_layer_neurons, activation='relu', input_dim=30))
+    model.add(Dense(units=hidden_layer_neurons, activation='relu', input_dim=attributes))
     model.add(Dropout(dropout))
 
     # warstwa wyjsciowa
--- temp.py
+++ temp.py
@@ -1,4 +1,7 @@
+import numpy
+
 import network
+
 
 data = network.read_data()
 train_data, train_labels, test_data, test_labels = network.separate_sets(data)
@@ -11,4 +14,15 @@ model.fit(x=train_data, y=train_labels, epochs=20, batch_size=32)
 print("testowanie:")
 evaluate = model.evaluate(x=test_data, y=test_labels)
 print("wynik:")
-print(evaluate[1])
+print(evaluate)
+
+result = numpy.zeros(shape=[50, 3])
+for x in range(49):
+    data = network.read_data()
+    train_data, train_labels, test_data, test_labels = network.separate_sets(data, test_set=(x+1)*0.02)
+    model = network.create_model()
+    model.fit(x=train_data, y=train_labels, epochs=10, batch_size=32)
+    evaluate = model.evaluate(x=test_data, y=test_labels)
+    result[x, :] = [x, evaluate[0], evaluate[1]]
+
+print(result)
